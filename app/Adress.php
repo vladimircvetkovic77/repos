71228e8e5a01b2fb81a30a4e9fe846b6a4d2cda6
Vladimir Cvetkovic <vladimircvetkovic77@icloud.com>
2019-11-22 11:17:38 +0100

    Final with validation rules in Model (out of controller)

--- app/Adress.php
+++ app/Adress.php
@@ -7,8 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Adress extends Model
 {
+	 protected $fillable = [
+    	'user_id',
+    	'line1'
+    ];
+
+    public static $createRules = [
+            'user_id' => 'required',
+            'line1' => 'required'
+        ];
+        
+    public static $updateRules = [
+            'user_id' => 'required',
+            'line1' => 'required'
+    ];
+
     public function user()
     {
     	return $this->belongsTo(User::class);
     }
+
+    
 }
--- app/Http/Controllers/AdressController.php
+++ app/Http/Controllers/AdressController.php
@@ -13,8 +13,19 @@ class AdressController extends Controller
     {
     	$this->adresses = $adresses;
     }
+
     public function index()
     {
     	dd($this->adresses);
+    }
+
+    public function create(Request $request)
+    {
+    	if ($request->validate(Adress::$createRules)) {
+            $this->adresses->create([
+        		'user_id'=> $request->id,
+        		'line1'=> $request->line1
+        	]);
+        }
     }
 }
